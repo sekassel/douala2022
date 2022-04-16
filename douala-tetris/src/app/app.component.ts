da5a3d4196a8d3f7bf72e4a16c81e4fc2da7cf1a
Albert Zündorf <zuendorf@uni-kassel.de>
2022-04-16 17:35:46 +0200

    starting rxdb

--- douala-tetris/src/app/app.component.ts
+++ douala-tetris/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { createRxDatabase } from 'rxdb';
+import { getRxStoragePouch, addPouchPlugin } from 'rxdb/plugins/pouchdb';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'douala-tetris';
+
+  ngOnInit(): void {
+    console.log("Starting app, creating database");
+
+  }
 }
--- douala-tetris/src/polyfills.ts
+++ douala-tetris/src/polyfills.ts
@@ -42,6 +42,14 @@
  *
  */
 
+ (window as any).global = window;
+ (window as any).process = {
+     env: { DEBUG: undefined },
+ };
+
+
+
+
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
