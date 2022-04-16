ecae66d363dc6d6ce65791a9e1a3482963f2b791
Albert Zündorf <zuendorf@uni-kassel.de>
2022-04-16 22:23:43 +0200

    score db initialized. Scores are stored.

--- douala-tetris/src/app/app.component.ts
+++ douala-tetris/src/app/app.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { createRxDatabase } from 'rxdb';
 import { getRxStoragePouch, addPouchPlugin } from 'rxdb/plugins/pouchdb';
+import { highScoreCollectionMethods, HighScoreDatabase, HighScoreDatabaseCollections, highScoreDocMethods, highScoreSchema } from './modules/scoredatabase/highscoresschema'
+import { RxDBDevModePlugin } from 'rxdb/plugins/dev-mode';
+import { ScoreserviceService } from './modules/scoredatabase/scoreservice.service'
+
+addPouchPlugin(require('pouchdb-adapter-idb'));
+console.log('pouchdb-adapter-idb added')
+
+
 
 @Component({
   selector: 'app-root',
@@ -8,10 +16,16 @@ import { getRxStoragePouch, addPouchPlugin } from 'rxdb/plugins/pouchdb';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+
   title = 'douala-tetris';
 
-  ngOnInit(): void {
-    console.log("Starting app, creating database");
+  constructor(
+    private scoreService: ScoreserviceService
+  ) {  }
 
+  async ngOnInit() {
+    await this.scoreService.init();
+    const amount = await this.scoreService.scoresDatabase?.scores.countAllDocuments();
+    console.log(`app has started and we have ${amount} scores in our database`)
   }
 }
---
+++ douala-tetris/src/app/modules/scoredatabase/highscoresschema.ts
@@ -0,0 +1,71 @@
+import {
+    toTypedRxJsonSchema,
+    ExtractDocumentTypeFromTypedRxJsonSchema,
+    RxJsonSchema,
+    RxDocument,
+    RxCollection,
+    RxDatabase
+} from 'rxdb/plugins/core';
+
+
+export const highScoreSchemaLiteral = {
+    title: 'high score schema',
+    description: 'describes a high score',
+    version: 0,
+    keyCompression: true,
+    primaryKey: 'dateTime',
+    type: 'object',
+    properties: {
+        dateTime: {
+            type: 'string'
+        },
+        score: {
+            type: 'string'
+        },
+        player: {
+            type: 'string'
+        }
+    },
+    required: ['dateTime', 'score'],
+    indexes: ['score']
+} as const; // <- It is important to set 'as const' to preserve the literal type
+export const schemaTyped = toTypedRxJsonSchema(highScoreSchemaLiteral);
+
+// aggregate the document type from the schema
+export type HighScoreDocType = ExtractDocumentTypeFromTypedRxJsonSchema<typeof schemaTyped>;
+
+// create the typed RxJsonSchema from the literal typed object.
+export const highScoreSchema: RxJsonSchema<HighScoreDocType> = highScoreSchemaLiteral;
+
+export type HighScoreDocMethods = {
+    scream: (v: string) => string;
+};
+
+export const highScoreDocMethods: HighScoreDocMethods = {
+    scream: function(this: HighScoreDocument, what: string) {
+        return this.dateTime + ' screams: ' + what.toUpperCase();
+    }
+};
+
+type HighScoreDocument = RxDocument<HighScoreDocType, HighScoreDocMethods>;
+
+// we declare one static ORM-method for the collection
+type HighScoreCollectionMethods = {
+    countAllDocuments: () => Promise<number>;
+}
+
+export const highScoreCollectionMethods: HighScoreCollectionMethods = {
+    countAllDocuments: async function(this: HighScoreCollection) {
+        const allDocs = await this.find().exec();
+        return allDocs.length;
+    }
+};
+
+// and then merge all our types
+export type HighScoreCollection = RxCollection<HighScoreDocType, HighScoreDocMethods, HighScoreCollectionMethods>;
+
+export type HighScoreDatabaseCollections = {
+    scores: HighScoreCollection
+}
+
+export type HighScoreDatabase = RxDatabase<HighScoreDatabaseCollections>;
