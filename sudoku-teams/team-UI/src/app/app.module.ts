b93e21f01c02c74cfeb62292f8cc26ef5b170b09
Grace-Code-Lover <gracenyegue@gmail.com>
2022-05-03 15:51:02 +0100

    commit 4

--- sudoku-teams/team-UI/src/app/app.module.ts
+++ sudoku-teams/team-UI/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CreatingTeamComponent } from './components/creating-team/creating-team.component';
 import { TeamsManagementComponent } from './components/teams-management/teams-management.component';
+import { ManageMyTeamComponent } from './components/manage-my-team/manage-my-team.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CreatingTeamComponent,
-    TeamsManagementComponent
+    TeamsManagementComponent,
+    ManageMyTeamComponent
   ],
   imports: [
     BrowserModule,
---
+++ sudoku-teams/team-UI/src/app/components/manage-my-team/manage-my-team.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-manage-my-team',
+  templateUrl: './manage-my-team.component.html',
+  styleUrls: ['./manage-my-team.component.scss']
+})
+export class ManageMyTeamComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
