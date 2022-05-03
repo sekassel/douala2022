043a8a6e2cfbd058dcd1cb231c711daab51d9e7b
Neambo <wilfridneambo63@gmail.com>
2022-05-03 13:41:52 +0100

    econd commit

--- sudoku-teams/team-UI/src/app/app-routing.module.ts
+++ sudoku-teams/team-UI/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CreatingTeamComponent } from './components/creating-team/creating-team.component';
+import { TeamsManagementComponent } from './components/teams-management/teams-management.component';
 
-const routes: Routes = [ { path: 'teams-management/creating-team',component:CreatingTeamComponent }]
+
+const routes: Routes = [ 
+  {path: 'manageTeams', component: TeamsManagementComponent},
+  { path: 'teams-management/creating-team',component:CreatingTeamComponent }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
