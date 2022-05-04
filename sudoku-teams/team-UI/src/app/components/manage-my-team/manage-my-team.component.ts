9b218ccf3d46989e3b26ae9c315e403d0d274d15
Grace-Code-Lover <gracenyegue@gmail.com>
2022-05-04 10:26:59 +0100

    commit 6

--- sudoku-teams/team-UI/src/app/components/manage-my-team/manage-my-team.component.ts
+++ sudoku-teams/team-UI/src/app/components/manage-my-team/manage-my-team.component.ts
@@ -6,16 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./manage-my-team.component.scss']
 })
 export class ManageMyTeamComponent implements OnInit {
-  myTeams:Array<String>
-  allTeams:Array<String>
-  createdTeams:Array<String>
+  requests:Array<String>
+  invitations:Array<String>
+  members:Array<String>
+  img:string
   constructor() {
-    this.myTeams = ["One of Grace's teams","One of Grace's teams","One of Grace's teams"]
-    this.allTeams = ["One of Grace's teams","One of Grace's teams","One of Grace's teams"]
-    this.createdTeams = ["One of Grace's teams","One of Grace's teams","One of Grace's teams"]
+    this.requests = ["Request from Grace","Request from Grace","Request from Grace"]
+    this.invitations = ["One of Grace's teams","One of Grace's teams","One of Grace's teams"]
+    this.members = ["One of Grace's teams","One of Grace's teams","One of Grace's teams"]
+    this.img = "27.jpg"
    }
 
   ngOnInit(): void {
   }
-
 }
