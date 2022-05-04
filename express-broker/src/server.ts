42896b9fda382dcdc9d052f186d04ae0133b7962
Albert Zündorf <zuendorf@uni-kassel.de>
2022-05-04 21:28:18 +0200

    changed to ts-node-dev app.post publish works

--- express-broker/src/server.ts
+++ express-broker/src/server.ts
@@ -2,7 +2,11 @@ import * as express from 'express';
 import * as http from 'http';
 import * as WebSocket from 'ws';
 
+
+import bodyParser = require('body-parser')
+
 const app = express();
+app.use(bodyParser.json());
 
 //initialize a simple http server
 const server = http.createServer(app);
@@ -11,6 +15,22 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 const wsList : WebSocket[] = []
 const topicMap: Map<string, WebSocket[]> = new Map();
+const eventMap: Map<string, any[]> = new Map();
+
+
+// let us have an post endpoint for publishing new events
+app.post('/publish', (req, res) => {
+    console.log
+    const jsonMsg = req.body;
+    console.log('post publish got body ' + JSON.stringify(jsonMsg, null, 3))
+    handlePublish(jsonMsg)
+    res.send('Thank you')
+})
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Douala2022 Event Broker.')
+})
+
 
 wss.on('connection', (ws: WebSocket) => {
 
@@ -24,58 +44,11 @@ wss.on('connection', (ws: WebSocket) => {
 
         if (msg.startsWith('{')) {
             const jsonMsg = JSON.parse(msg);
-            if (jsonMsg.topic == 'subscribe') {
-                // console.log('it is a subscribe for topic  ' + jsonMsg.targetTopic);
-                const tgtTopic = jsonMsg.targetTopic;
-                var subscribers = topicMap.get(tgtTopic);
-                if (subscribers == null) {
-                    subscribers = [];
-                    topicMap.set(tgtTopic, subscribers)
-                }
-                subscribers.push(ws);
-                const answer = {
-                    state: 'success',
-                    originalMessage: jsonMsg
-                }
-                const text = JSON.stringify(answer, null, 3)
-                ws.send(text, (err) => {
-                    if (err) {
-                        console.log(`send error ` + JSON.stringify(err))
-                    }
-                })
-                // console.log('reply to subscribe has been  \n' + text);
-                return
-            }
-
-            if (jsonMsg.topic == 'publish') {
-                // console.log('it is a publish for topic  ' + jsonMsg.targetTopic);
-                const tgtTopic = jsonMsg.targetTopic;
-
-                // find interested sockets
-                const socketList = topicMap.get(jsonMsg.targetTopic)
-                if (socketList == null) {
-                    console.log("socket list for this topic is empty")
-                    return
-                }
-
-                // send the message
-                const answer = {
-                    topic: tgtTopic,
-                    payload: jsonMsg.payload
-                }
-                const text = JSON.stringify(answer, null, 3)
-                for (const s of socketList) {
-                    s.send(text, (err) => {
-                        if (err) {
-                            console.log(`send error ` + JSON.stringify(err))
-                        }
-                    })
-                    console.log('have send answer to some service');
-                }
-
-                return
-            }
+            handleSubscribe(ws, jsonMsg)
+            handlePublish(jsonMsg)
+            return
         }
+
 
         ws.send(`Hello, you sent -> ${message}`);
     });
@@ -89,3 +62,70 @@ server.listen(process.env.PORT || 3333, () => {
     const port = server.address()
     console.log(`Server started on port ${JSON.stringify(server?.address())} :)`);
 });
+
+
+function handleSubscribe(ws: WebSocket, jsonMsg: any) {
+    if (jsonMsg.topic == 'subscribe') {
+        // console.log('it is a subscribe for topic  ' + jsonMsg.targetTopic);
+        const tgtTopic = jsonMsg.targetTopic;
+        var subscribers = topicMap.get(tgtTopic);
+        if (subscribers == null) {
+            subscribers = [];
+            topicMap.set(tgtTopic, subscribers)
+        }
+        subscribers.push(ws);
+        const answer = {
+            state: 'success',
+            originalMessage: jsonMsg
+        }
+        const text = JSON.stringify(answer, null, 3)
+        ws.send(text)
+        // console.log('reply to subscribe has been  \n' + text);
+        return
+    }
+}
+
+function handlePublish(jsonMsg: any) {
+    if (jsonMsg.topic == 'publish') {
+        // console.log('it is a publish for topic  ' + jsonMsg.targetTopic);
+        const tgtTopic = jsonMsg.targetTopic;
+
+        // find interested sockets
+        const socketList = topicMap.get(jsonMsg.targetTopic)
+        if (socketList == null) {
+            console.log("socket list for this topic is empty")
+            return
+        }
+
+        // send the message
+        const answer = {
+            topic: tgtTopic,
+            payload: jsonMsg.payload
+        }
+        const text = JSON.stringify(answer, null, 3)
+
+        // store in eventMap
+        var eventList = eventMap.get(answer.topic);
+        if ( ! eventList) {
+            eventList = []
+            eventMap.set(answer.topic, eventList)
+        }
+        eventList.push(answer)
+
+
+        // send to subscribers
+        for (const s of socketList) {
+            s.send(text, (err) => {
+                if (err) {
+                    const errString = JSON.stringify(err)
+                    if (errString != '{}') {
+                        console.log(`send error ` + JSON.stringify(err))
+                    }
+                }
+            })
+            console.log('have send answer to some service');
+        }
+
+        return
+    }
+}
