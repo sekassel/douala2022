47db377db88a9b6fe72aa5fe2780d93f37c13b03
Grace-Code-Lover <gracenyegue@gmail.com>
2022-05-04 15:07:28 +0100

    components UI

--- sudoku-teams/team-UI/src/app/components/manage-my-team/manage-my-team.component.ts
+++ sudoku-teams/team-UI/src/app/components/manage-my-team/manage-my-team.component.ts
@@ -9,12 +9,10 @@ export class ManageMyTeamComponent implements OnInit {
   requests:Array<String>
   invitations:Array<String>
   members:Array<String>
-  img:string
   constructor() {
     this.requests = ["Request from Grace","Request from Grace","Request from Grace"]
-    this.invitations = ["One of Grace's teams","One of Grace's teams","One of Grace's teams"]
-    this.members = ["One of Grace's teams","One of Grace's teams","One of Grace's teams"]
-    this.img = "27.jpg"
+    this.invitations = ["User Grace Enyegue","User Grace Enyegue","User Grace Enyegue"]
+    this.members = ["User Grace Enyegue","User Grace Enyegue","User Grace Enyegue"]
    }
 
   ngOnInit(): void {
