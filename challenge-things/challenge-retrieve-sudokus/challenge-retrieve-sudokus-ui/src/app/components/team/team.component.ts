a77c4626b106d2d25c61af8b42cf5c6964fd166c
atetheone <atevirran@gmail.com>
2022-05-05 15:33:47 +0100

    adding post request for invitation

--- challenge-things/challenge-retrieve-sudokus/challenge-retrieve-sudokus-ui/src/app/components/team/team.component.ts
+++ challenge-things/challenge-retrieve-sudokus/challenge-retrieve-sudokus-ui/src/app/components/team/team.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
@@ -11,12 +12,7 @@ import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 export class TeamComponent implements OnInit {
 
 
-  invitation = {
-    username: "",
-    challengeId: "AEZ2",
-    teamInvited: "Marquez",
-    time: new Date()
-  };
+
 
   challenges = [
     {
@@ -45,17 +41,54 @@ export class TeamComponent implements OnInit {
   @Input()
   challengeLost!: number;
 
+  modalRef!: any;
 
   constructor(
     protected modalService: NgbModal,
-    protected vref:ViewContainerRef
+    private http: HttpClient
   ) {
   }
   ngOnInit(): void {
   }
 
   open(content: any) {
-    const modalRef = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'})
+    this.modalRef = this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'});
+    this.modalRef.result
+      .then(
+        (result: any) => {
+          console.log(`Result: ${result}`);
+          
+        }, 
+        (reason: any) => {
+          console.log(`Reason: ${reason}`);
+          
+        });
+
+  }
+
+  async sendInvitation(content: any) {
+    const payload = {
+      username: "",
+      challengeId: "AEZ2",
+      teamInvited: "Marquez",
+      time: new Date()
+    };
+
+    const invitation = {
+      topic: 'publish',
+      targetTopic: 'invite',
+      payload: payload
+    };
+
+
+    await this.http.post<any>('http://localhost:3000/invite', invitation)
+      .subscribe(
+        data => {
+          console.log(`invitatio sent:  \n${JSON.stringify(data, null, 3)}`);
+          this.modalRef.close();
+        },
+        err => console.log(`error from post invitation \n ${JSON.stringify(err, null, 3)}`)
+      );
 
   }
 
