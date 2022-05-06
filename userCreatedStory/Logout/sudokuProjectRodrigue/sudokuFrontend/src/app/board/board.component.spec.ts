2694af5409519a5ffff0fa4c49974f27a53c86dd
Rodrigue Mbiabo <rodriguembiabo@gmail.com>
2022-05-06 09:41:55 +0100

    hi

---
+++ userCreatedStory/Logout/sudokuProjectRodrigue/sudokuFrontend/src/app/board/board.component.spec.ts
@@ -0,0 +1,28 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {MatDialogModule} from '@angular/material/dialog';
+import { BoardComponent } from './board.component';
+
+describe('BoardComponent', () => {
+  let component: BoardComponent;
+  let fixture: ComponentFixture<BoardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ BoardComponent ],
+      imports: [
+          MatDialogModule
+      ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(BoardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
