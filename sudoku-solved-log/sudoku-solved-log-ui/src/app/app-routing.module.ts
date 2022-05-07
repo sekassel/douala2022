2a3e62770c7794645c756afd42612fb038ba39ca
atetheone <atevirran@gmail.com>
2022-05-07 20:11:59 +0100

    update footer

--- sudoku-solved-log/sudoku-solved-log-ui/src/app/app-routing.module.ts
+++ sudoku-solved-log/sudoku-solved-log-ui/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SudokuLogComponent } from './components/sudoku-log/sudoku-log.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: SudokuLogComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- sudoku-solved-log/sudoku-solved-log-ui/src/app/app.module.ts
+++ sudoku-solved-log/sudoku-solved-log-ui/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SudokuLogComponent } from './components/sudoku-log/sudoku-log.component';
+import { TeamComponent } from './components/team/team.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SudokuLogComponent,
+    TeamComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
